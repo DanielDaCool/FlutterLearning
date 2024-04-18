0155a3321f972ad2a2f3d0410b267172e7cd465c
DanielDaMagniv <danielvfx19@gmail.com>
2024-04-18 21:34:07 +0300

    works yay

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,26 @@
 
 import 'package:flutter/material.dart';
 
+const Color backgroundColor = Color.fromARGB(255, 34, 34, 34);
 
 void main() {
   runApp(const MyApp());
   
 }
+
+String getPage(int pageValue){
+  String str = "";
+  switch (pageValue){
+    case 0:
+      str = "Home";
+      break;
+    case 1:
+      str = "Pages";
+      break;
+  }
+  return str;
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,11 +30,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: backgroundColor,
         appBar: AppBar(
           title: const Text("Main"),
           backgroundColor: Colors.green,
         ),
-        body: ,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.pages),
+              label: "Pages"
+            )
+          ],
+          onTap: (value) => print("Clicked on page " + getPage(value)),
+        ),
         
       ),
     );
