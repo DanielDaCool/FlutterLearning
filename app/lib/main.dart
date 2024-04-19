50d7ef956ab6f5cde435237d32fa9aa8d4a4263e
DanielDaMagniv <danielvfx19@gmail.com>
2024-04-19 17:11:44 +0300

    added second page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,43 +1,53 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 const Color backgroundColor = Color.fromARGB(255, 34, 34, 34);
 
 void main() {
-  runApp(const MyApp());
+  HomePage.initState();
+  runApp(const HomePage());
 }
 
 String userName = "";
 
-String getPage(int pageValue) {
-  String str = "";
-  switch (pageValue) {
+int currentPage = 0;
+
+void getPageGoToPage(int pageValue, BuildContext context) {
+  
+  if(pageValue == currentPage) return;
+  switch(pageValue){
     case 0:
-      str = "Home";
+      
+      Navigator.push(context, MaterialPageRoute(builder: (_) => const HomePage()));
       break;
     case 1:
-      str = "Pages";
+      Navigator.push(context, MaterialPageRoute(builder: (_) => const secondPage()));
       break;
+    
+      
   }
-  return str;
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<HomePage> createState() => _HomePageState();
 
-  @override
-  void initState() {
+  static void initState() {
     userName = "";
   }
 }
 
-class _MyAppState extends State<MyApp> {
+class _HomePageState extends State<HomePage> {
   // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
+    currentPage = 0;
     return MaterialApp(
       darkTheme: ThemeData.dark(),
       home: Builder(builder: (context) {
@@ -52,20 +62,21 @@ class _MyAppState extends State<MyApp> {
               BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
               BottomNavigationBarItem(icon: Icon(Icons.pages), label: "Pages")
             ],
-            onTap: (value) => print("Clicked on page " + getPage(value)),
+            onTap: (value) => (getPageGoToPage(value, context)),
           ),
           drawer: Drawer(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                
-                TextField(
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                 child:  TextField(
                   autocorrect: true,
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(),
                     labelText: 'Enter your username',
-                    
-                    
                   ),
                   onSubmitted: (value) {
                     setState(() {
@@ -73,6 +84,8 @@ class _MyAppState extends State<MyApp> {
                     });
                   },
                 ),
+                ),
+                
                 TextButton.icon(
                   onPressed: () {},
                   icon: const Icon(Icons.search),
@@ -82,12 +95,53 @@ class _MyAppState extends State<MyApp> {
                     onPressed: () => setState(() {
                           userName = "";
                         }),
-                    child: Text("Log out")),
+                    child: const Text("Log out")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => secondPage()));
+                    },
+                    child: Text("go to page 2")),
               ],
             ),
           ),
         );
       }),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class secondPage extends StatelessWidget {
+  const secondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    currentPage = 1;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => const HomePage()));
+                  },
+                  icon: const Icon(Icons.arrow_back)),
+              const Text("Page 2"),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.green,
+        bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.pages), label: "Pages")
+            ],
+            onTap: (value) => (getPageGoToPage(value, context)),
+          ),
+      ),
+      
     );
   }
 }
